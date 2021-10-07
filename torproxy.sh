86b7e2c4ebb938b4bb7215c228fc11e7eb96ecfb
Oliver Gugger <gugger@gmail.com>
2021-10-07 09:43:16 +0200

    Remove privoxy

--- torproxy.sh
+++ torproxy.sh
@@ -219,6 +219,5 @@ else
     [[ -e /srv/tor/hidden_service/hostname ]] && {
         echo -en "\nHidden service hostname: "
         cat /srv/tor/hidden_service/hostname; echo; }
-    /usr/sbin/privoxy --user privoxy /etc/privoxy/config
     exec /usr/bin/tor
 fi
